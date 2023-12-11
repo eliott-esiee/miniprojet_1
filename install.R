5e412d03ca2510df17c357c0f23fb623b25e2879
eliott-esiee <eliott.vigier@edu.esiee.fr>
2023-12-11 18:02:37 +0100

    1

---
+++ install.R
@@ -0,0 +1,10 @@
+install.packages("dplyr")
+install.packages("tm")
+install.packages("knitr")
+
+install.packages("tokenizers")
+install.packages("text2vec")
+
+install.packages("ggplot2")
+
+install.packages("kableExtra")
